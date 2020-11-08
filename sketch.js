928fe5c5e0c4f0e7f4d6b58dd111d80422e8e30d
Nihar1015 <69799996+Nihar1015@users.noreply.github.com>
2020-11-08 23:42:48 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -33,10 +33,7 @@ function draw() {
     {
       wall.shapeColor=color(255,0,0);
     }
-    if(damage<180 && deformation>100)
-    {
-      bullet.shapeColor=color(230,230,0);
-    }
+    
     if(damage<10)
     {
       wall.shapeColor=color(0,255,0);
